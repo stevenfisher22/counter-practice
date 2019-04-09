9423071785ecebfb18d7189ba38a9cedae4923bb
Steven Fisher <steven.fisher22@gmail.com>
2019-04-09 17:41:12 -0500

    buttons work - plain react app done

--- src/Counter.js
+++ src/Counter.js
@@ -5,14 +5,26 @@ class Counter extends React.Component {
         count: 0
     };
 
+    increment = () => {
+        this.setState(state => ({
+            count: state.count + 1
+        }))
+    }
+
+    decrement = () => {
+        this.setState(state => ({
+            count: state.count - 1
+        }))
+    }
+
     render() {
         return (
             <div className="counter">
                 <h2>Counter</h2>
                 <div>
-                    <button>-</button>
-                    <span className="count">0</span>
-                    <button>+</button>
+                    <button onClick={this.decrement}>-</button>
+                    <span className="count">{this.state.count}</span>
+                    <button onClick={this.increment}>+</button>
                 </div>
             </div>
         )
