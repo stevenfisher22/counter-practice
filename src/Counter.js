14cdba77740d71dedfa2d6db00560087885372e3
Steven Fisher <steven.fisher22@gmail.com>
2019-04-09 17:38:08 -0500

    css initialized

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 class Counter extends React.Component {
+    state = {
+        count: 0
+    };
+
     render() {
-        return "Counter"
+        return (
+            <div className="counter">
+                <h2>Counter</h2>
+                <div>
+                    <button>-</button>
+                    <span className="count">0</span>
+                    <button>+</button>
+                </div>
+            </div>
+        )
     }
 }
 
